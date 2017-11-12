78ceca1c6747a76f73593a6cfbeec235d194a6ec
DESKTOP-DBAP0RR\JustinYang <1301f17@gmail.com>
2017-11-12 13:45:55 -0500

    bug fix and cross validation

--- detection.py
+++ detection.py
@@ -4,6 +4,7 @@ import os
 from PIL import Image
 import tensorflow as tf
 import random
+import matplotlib.pyplot as plt
 
 H = 27
 W = 27
@@ -19,17 +20,21 @@ def save_data():
 	data_folder = './CRCHistoPhenotypes_2016_04_28/Detection/'
 	xs1 = []
 	ys1 = []
+	fs1 = []
 	xs2 = []
 	ys2 = []
+	fs2 = []
 
 	turn = True
 
 	sub_folders = [x[0] for x in os.walk(data_folder)][1:]
+	random.shuffle(sub_folders)
 	for sub_folder in sub_folders:
 		file_prefix = sub_folder.split('/')[-1]
 		image_data = rgb2gray(np.array(Image.open(sub_folder + '/' + file_prefix + '.bmp')))
 		labels = sio.loadmat(sub_folder + '/' + file_prefix + "_detection.mat")['detection']
-		num_1 = 0
+
+		# load patches with nucleus as center(label=1)
 		for label in labels:
 			if int(label[0]) > (W-1)/2 and int(label[0]) < 500 - (W-1)/2 and int(label[1]) > (H-1)/2 and int(label[1]) < 500 - (H-1)/2:
 				if turn:
@@ -37,54 +42,63 @@ def save_data():
 					ys1.append(1)
 				else:
 					xs2.append(image_data[int(label[1]-(H-1)/2):int(label[1]+(H-1)/2 + 1), int(label[0]-(W-1)/2):int(label[0]+(W-1)/2) + 1])
-					ys2.append(1)					
-				num_1 += 1
+					ys2.append(1)				
 
+		# load patches without nucleus as center(label=0)
 		num_0 = 0
-		iteration = 0
-		while num_0 < num_1 and iteration < 10000:
-			x = random.randint(0,499)
-			y = random.randint(0,499)
-			flag = True
-			for label in labels:
-				if abs(label[0]-x) < 3 or abs(label[1]-y) < 3:
-					flag = False
-			if flag:
-				if turn:
-					xs1.append(image_data[int(y-(H-1)/2):int(y+(H-1)/2 + 1), int(x-(W-1)/2):int(x+(W-1)/2 + 1)])
-					ys1.append(0)
-					print(image_data[int(y-(H-1)/2):int(y+(H-1)/2 + 1), int(x-(W-1)/2):int(x+(W-1)/2 + 1)].shape)
-				else:
-					xs2.append(image_data[int(y-(H-1)/2):int(y+(H-1)/2 + 1), int(x-(W-1)/2):int(x+(W-1)/2 + 1)])
-					ys2.append(0)					
-				num_0 += 1
-			iteration += 1
+		for x in range(int((W-1)/2), int(500 - (W-1)/2 - 1), 5):
+			for y in range(int((H-1)/2), int(500 - (H-1)/2 - 1), 5):
+				flag = True
+				for label in labels:
+					if abs(label[0] - x) < 4 or abs(label[1] - y) < 4:
+						flag = False
+				if flag:
+					if turn:
+						xs1.append(image_data[int(y-(H-1)/2):int(y+(H-1)/2 + 1), int(x-(W-1)/2):int(x+(W-1)/2 + 1)])
+						ys1.append(0)
+					else:
+						xs2.append(image_data[int(y-(H-1)/2):int(y+(H-1)/2 + 1), int(x-(W-1)/2):int(x+(W-1)/2 + 1)])
+						ys2.append(0)
+					num_0 += 1
+				if num_0 > 1000:
+					break
+
+		if turn:
+			fs1.append(sub_folder)
+		else:
+			fs2.append(sub_folder)
 		turn = not turn
 		print(sub_folder)
 	xs1 = np.array(xs1)
 	ys1 = np.array(ys1)
+	fs1 = np.array(fs1)
 	xs2 = np.array(xs2)
 	ys2 = np.array(ys2)
-	print(xs1.shape)
+	fs2 = np.array(fs2)
+
+
 	np.save('xs1.npy', xs1)
 	np.save('ys1.npy', ys1)
+	np.save('fs1.npy', fs1)
 	np.save('xs2.npy', xs2)
 	np.save('ys2.npy', ys2)
+	np.save('fs2.npy', fs2)
 
 
 def load_data():
 	xs1 = np.load('xs1.npy')
 	ys1 = np.load('ys1.npy')
+	fs1 = np.load('fs1.npy')
 	xs2 = np.load('xs2.npy')
 	ys2 = np.load('ys2.npy')
-	return (xs1, ys1, xs2, ys2)
+	fs2 = np.load('fs2.npy')
+	return (xs1, ys1, fs1, xs2, ys2, fs2)
 
 
 
-save_data()
+# save_data()
 
-# (xs1, ys1, xs2, ys2) = load_data()
-# print(xs1[0:2])
+# (xs1, ys1, fs1, xs2, ys2, fs2) = load_data()
 # print(len(ys1))
 # print(sum(ys1))
 # print(len(ys2))
@@ -94,7 +108,7 @@ save_data()
 # H = 27
 # W = 27
 
-# X = tf.placeholder(tf.float32, [None, H, W], name='X')/255
+# X = tf.placeholder(tf.float32, [None, H, W], name='X')
 # Y = tf.placeholder(tf.float32, [None, 1], name='Y')
 # keep_prob = tf.placeholder(tf.float32, name='keep_prob') # dropout (keep probability)
 
@@ -149,47 +163,165 @@ save_data()
 # mp2 = maxpool2d(c2)
 # mp2_flat = tf.reshape(mp2, [-1, 5*5*48])
 # f1 = fuc2d(mp2_flat, weights['f1'], bias['f1'])
-# output = tf.nn.sigmoid(tf.matmul(f1, weights['f2']) + bias['f2'])
+# output = tf.nn.sigmoid(tf.matmul(f1, weights['f2']) + bias['f2'], name='output')
+# # print(c1.get_shape())
+# # print(mp1.get_shape())
+# # print(c2.get_shape())
+# # print(mp2.get_shape())
+# # print(mp2_flat.get_shape())
+# # print(f1.get_shape())
+# # print(output.get_shape())
 
 
-# loss = tf.reduce_mean(-tf.reduce_sum(Y * tf.log(output), reduction_indices=[1]))
+
+# loss = tf.reduce_mean(-tf.reduce_sum(Y * tf.log(output) + (1-Y) * tf.log(1 - output), reduction_indices=[1]))
 
 # # train_step = tf.train.MomentumOptimizer(0.0001, 0.9).minimize(loss)
 # train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
 
 
-# # for training and save model
+# for training and save model
 # sess = tf.Session()
-# init = tf.initialize_all_variables()
+# init = tf.global_variables_initializer()
 # sess.run(init)
 # saver = tf.train.Saver()
 
-# for i in range(120):
+# for i in range(300):
 #     # training
-#     sess.run(train_step, feed_dict={X: xs1, Y: ys1, keep_prob:0.8})
+#     indices = np.random.choice(len(xs1), 5000, replace=False)
+#     sess.run(train_step, feed_dict={X: xs1[indices].reshape(-1, H, W, 1), Y: ys1[indices].reshape(-1, 1), keep_prob:0.8})
 #     if i % 10 == 0:
 #         # to see the step improvement
-#         print(sess.run(loss, feed_dict={X: xs1, Y: ys1, keep_prob:0.8}))
+#         print(sess.run(loss, feed_dict={X: xs1[indices].reshape(-1, H, W, 1), Y: ys1[indices].reshape(-1, 1), keep_prob:0.8}))
+#         # print(sess.run(output, feed_dict={X: xs1[indices].reshape(-1, H, W, 1), keep_prob:0.8}))
 
-# saver.save(sess, './my-model')
 
-# # for load model and testing
+# saver.save(sess, './model1/model1')
+# sess.close()
+
 # sess = tf.Session()
-# saver = tf.train.import_meta_graph('./my-model.meta')
-# saver.restore(sess,tf.train.latest_checkpoint('./'))
+# init = tf.global_variables_initializer()
+# sess.run(init)
+# saver = tf.train.Saver()
 
-# graph = tf.get_default_graph()
-# output = graph.get_tensor_by_name("output:0")
-# X = graph.get_tensor_by_name("X:0")
-# Y = graph.get_tensor_by_name("Y:0")
-# keep_prob = graph.get_tensor_by_name("keep_prob:0")
+# for i in range(300):
+#     # training
+#     indices = np.random.choice(len(xs2), 5000, replace=False)
+#     sess.run(train_step, feed_dict={X: xs2[indices].reshape(-1, H, W, 1), Y: ys2[indices].reshape(-1, 1), keep_prob:0.8})
+#     if i % 10 == 0:
+#         # to see the step improvement
+#         print(sess.run(loss, feed_dict={X: xs2[indices].reshape(-1, H, W, 1), Y: ys2[indices].reshape(-1, 1), keep_prob:0.8}))
 
-# print(sess.run(output,feed_dict={X: xs[[0]], Y: ys[[0]], keep_prob:0.8}))
+# saver.save(sess, './model2/model2')
+# sess.close()
 
-# test_image = './CRCHistoPhenotypes_2016_04_28/Detection/img93/img93.bmp'
-# image_data = rgb2gray(np.array(Image.open(test_image)))
+# for load model and do the cross validation
+TP = 0
+TP_FN = 0
+TP_FP = 0
+threshold = 0.98
 
-# for i in range(0, image_data.shape[0] - W):
-# 	for j in range(0, image_data.shape[1] - H):
-# 		patch = image_data[i:i+W, j:j+H]
+sess = tf.Session()
+saver = tf.train.import_meta_graph('./model1/model1.meta')
+saver.restore(sess,tf.train.latest_checkpoint('./model1/'))
+
+graph = tf.get_default_graph()
+output = graph.get_tensor_by_name("output:0")
+X = graph.get_tensor_by_name("X:0")
+Y = graph.get_tensor_by_name("Y:0")
+keep_prob = graph.get_tensor_by_name("keep_prob:0")
+
+
+fs2 = np.load('fs2.npy')
+for sub_folder in fs2:
+	print(sub_folder)
+	image_path = sub_folder + '/' + sub_folder.split('/')[-1] + '.bmp'
+	label_path = sub_folder + '/' + sub_folder.split('/')[-1] + '_detection.mat'
+	image_data = rgb2gray(np.array(Image.open(image_path)))
+	labels = sio.loadmat(label_path)['detection']
+
+	result = np.zeros([500,500])
+
+	for y in range(int((H-1)/2), int(500 - (H-1)/2 - 1)):
+		patches = []
+		for x in range(int((W-1)/2), int(500 - (W-1)/2 - 1)):
+			patch = image_data[int(y-(H-1)/2):int(y+(H-1)/2 + 1), int(x-(W-1)/2):int(x+(W-1)/2) + 1]
+			patches.append(patch)
+		patches = np.array(patches)
+		result[y][int((H-1)/2): int(500 - (H-1)/2 - 1)] = sess.run(output,feed_dict={X: patches, keep_prob:0.8}).reshape(1, -1)
+
+	# filter the result so that only those local max and greater than some threshold will be positive.
+	result_filtered = np.zeros([500,500])
+	for y in range(6, result.shape[0]-7):
+		for x in range(6, result.shape[1]-7):
+			if result[y,x] == np.amax(result[y-6:y+7,x-6:x+7]) and result[y,x] > threshold:
+				result_filtered[y,x] = 1
+
+	# calculate TP FP FN for precision-recall
+	for label in labels:
+		if int(label[0]) > 6 and int(label[0]) < 493 and int(label[1]) > 6 and int(label[1]) < 393:
+			TP_FN += 1
+			if sum(sum(result_filtered[int(label[1])-6:int(label[1])+7, int(label[0])-6:int(label[0])+7])) > 0: # there exists at least one positive at the region of radius=6
+				TP += 1
+	TP_FP += sum(sum(result_filtered))
+
+sess.close()
+tf.reset_default_graph()
+
+
+
+sess = tf.Session()
+saver = tf.train.import_meta_graph('./model2/model2.meta')
+saver.restore(sess,tf.train.latest_checkpoint('./model2/'))
+
+graph = tf.get_default_graph()
+output = graph.get_tensor_by_name("output:0")
+X = graph.get_tensor_by_name("X:0")
+Y = graph.get_tensor_by_name("Y:0")
+keep_prob = graph.get_tensor_by_name("keep_prob:0")
+
+
+fs1 = np.load('fs1.npy')
+for sub_folder in fs1:
+	print(sub_folder)
+	image_path = sub_folder + '/' + sub_folder.split('/')[-1] + '.bmp'
+	label_path = sub_folder + '/' + sub_folder.split('/')[-1] + '_detection.mat'
+	image_data = rgb2gray(np.array(Image.open(image_path)))
+	labels = sio.loadmat(label_path)['detection']
+
+	result = np.zeros([500,500])
+
+	for y in range(int((H-1)/2), int(500 - (H-1)/2 - 1)):
+		patches = []
+		for x in range(int((W-1)/2), int(500 - (W-1)/2 - 1)):
+			patch = image_data[int(y-(H-1)/2):int(y+(H-1)/2 + 1), int(x-(W-1)/2):int(x+(W-1)/2) + 1]
+			patches.append(patch)
+		patches = np.array(patches)
+		result[y][int((H-1)/2): int(500 - (H-1)/2 - 1)] = sess.run(output,feed_dict={X: patches, keep_prob:0.8}).reshape(1, -1)
+
+	# filter the result so that only those local max and greater than some threshold will be positive.
+	result_filtered = np.zeros([500,500])
+	for y in range(6, result.shape[0]-7):
+		for x in range(6, result.shape[1]-7):
+			if result[y,x] == np.amax(result[y-6:y+7,x-6:x+7]) and result[y,x] > threshold:
+				result_filtered[y,x] = 1
+
+	# calculate TP FP FN for precision-recall
+	for label in labels:
+		if int(label[0]) > 6 and int(label[0]) < 493 and int(label[1]) > 6 and int(label[1]) < 493:
+			TP_FN += 1
+			if sum(sum(result_filtered[int(label[1])-6:int(label[1])+7, int(label[0])-6:int(label[0])+7])) > 0: # there exists at least one positive at the region of radius=6
+				TP += 1
+	TP_FP += sum(sum(result_filtered))
+
+sess.close()
+
+
+
+
+print(TP/TP_FN)
+print(TP/TP_FP)
+print(2*TP/(TP_FN+TP_FP))
+
+
 
